61f07f7cdc33e1a620a1ef69feb2510d9cce4d65
Tomasz Wrona <tomasz@wrona.me>
2018-12-01 23:23:02 +0100

    Added baseline tracking

--- utils/metrics.py
+++ utils/metrics.py
@@ -8,6 +8,7 @@ class Metrics(object):
         self.episode = 0
         self.episode_rewards = []
         self.episodes_rewards = []
+        self.baseline = 0
 
     def next_step(self, reward: float):
         self.episode_rewards.append(reward)
@@ -21,8 +22,21 @@ class Metrics(object):
         self.episodes_rewards.append(self.episode_reward())
         self.episode_rewards = []
 
+    def last_step(self):
+        return self.step - 1
+
+    def last_episode(self):
+        return self.episode - 1
+
     def episode_reward(self) -> float:
         return sum(self.episode_rewards)
 
+    def last_episode_reward(self) -> float:
+        return self.episodes_rewards[-1]
+
     def mean_episode_reward(self, horizon: int) -> float:
         return mean(self.episodes_rewards[-horizon:])
+
+    def new_baseline(self, value: float):
+        self.baseline = value
+        return self
